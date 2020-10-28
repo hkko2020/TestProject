35e4cdd5245b8bce3080e5664cc4854f33e0463a
hkko <hkko@CurieNote>
2020-10-28 14:16:19 -0700

    First commit.

---
+++ TeamManager/src/TestSample.java
@@ -0,0 +1,10 @@
+
+public class TestSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("It's a manager project");
+		System.out.println("This is second line.");
+	}
+
+}
